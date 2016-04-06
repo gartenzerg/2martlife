1b20b220898750702283079189440a522e36b84e
gartenzerg <gartenzerg@users.noreply.github.com>
2016-04-06 15:27:00 +0200

    Update send_mail.py

--- modules/email_receive/send_mail.py
+++ modules/email_receive/send_mail.py
@@ -1,11 +1,11 @@
 import smtplib
 
 fromaddr = 'mypi.balster@gmail.com'
-toaddr = 's.balster@gmx.net'
+toaddr = 'test@gmx.net'
 msg = 'TesT'
 
 username = 'mypi.balster@gmail.com'
-password = 'raspberry_1'
+password = ******
 
 server = smtplib.SMTP("smtp.gmail.com:587")
 server.starttls()
